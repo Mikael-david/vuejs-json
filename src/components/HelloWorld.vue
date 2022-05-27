328a29a0933ab649555699c2e696a2102a339141
Mikaël <mikael.david@gmail.com>
2022-05-27 16:18:12 +0200

    Add files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-for="item of items.items" :key="item.id">
+        {{ item.id }}
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      items: [],
+    }
+  },
+  async created() {
+    try {
+      const res = await axios.get(`../data.json`);
+      this.items = res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
